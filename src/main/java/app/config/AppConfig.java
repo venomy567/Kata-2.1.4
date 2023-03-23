7f2d953487c0cb38ee85744b955e30eb4b35f408
venomy567 <47823187+venomy567@users.noreply.github.com>
2023-03-23 20:57:50 +0300

    Ppitka 2

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,5 +1,7 @@
 package app.config;
 
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
